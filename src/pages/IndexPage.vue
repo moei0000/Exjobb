9dff824cd0eb8eb79ce5c3e0d2fa2fff2c6a2604
Andreas <andreas.sanz@hotmail.com>
2024-03-06 14:35:29 +0100

    Added Quasar

---
+++ src/pages/IndexPage.vue
@@ -0,0 +1,104 @@
+<template>
+<div class="wrapper">
+    <!-- Title -->
+    <header>
+       <h1 style="font-size: 29px; color: green;">Greenmapper</h1>
+    </header>
+    
+    <div class="stepper">
+      
+      <q-stepper 
+        v-model="step"
+        ref="stepper"
+        color="primary"
+        animated
+      >
+        <q-step
+          :name="1"
+          title="Map"
+          prefix="1"
+          :done="step > 1"
+        >
+          <!-- <LeafletMap /> -->
+          hej
+        </q-step>
+  
+        <q-step
+          :name="2"
+          title="Description"
+          prefix="2"
+          :done="step > 2"
+        >
+          <!-- <Description /> -->
+        </q-step>
+  
+        <q-step
+          :name="3"
+          title="Rating"
+          prefix="3"
+          :done="step > 3"
+        >
+          <!-- <Rating /> -->
+        </q-step>
+  
+        <q-step
+          :name="4"
+          title="Visit freq"
+          prefix="4"
+          :done="step > 4"
+        >
+          <!-- <VisitFreq /> -->
+        </q-step>
+  
+        <q-step
+          :name="5"
+          title="Activities"
+          prefix="5"
+          :done="step > 5"
+        >
+          <!-- <Activities /> -->
+        </q-step>
+  
+        <q-step
+          :name="6"
+          title="Priorities"
+          prefix="6"
+          :done="step > 6"
+        >
+          <!-- <Priorities /> -->
+        </q-step>
+  
+        <q-step
+          :name="7"
+          title="Donate"
+          prefix="7"
+          :done="step > 7"
+        >
+          <!-- <Donate /> -->
+        </q-step>
+    <template v-slot:navigation>
+      <q-stepper-navigation>
+        <q-btn @click="$refs.stepper.next()" color="primary" :label="step === 7 ? 'Finish' : 'Next'" />
+        <q-btn v-if="step > 1" flat color="primary" @click="$refs.stepper.previous()" label="Back" class="q-ml-sm" />
+      </q-stepper-navigation>
+    </template>
+    </q-stepper>
+    </div>
+  </div>
+</template>
+
+<style>
+
+</style>
+
+<script>
+import { ref } from 'vue'
+
+export default {
+  setup () {
+    return {
+      step: ref(1)
+    }
+  }
+}
+</script>
---
+++ src/pages/Rating.vue
@@ -0,0 +1,21 @@
+<template>
+    <h1>Rate your area</h1>
+
+    <b>(Score: 1 = bad, 6 = average, 10 = perfect)</b>
+
+    <p></p>
+    <v-rating
+        hover
+        :length="10"
+        :size="32"
+        :model-value="0"
+        active-color="primary"
+    />
+</template>
+
+<style>
+h1 {
+    color: green;
+    font-size: 13pt;
+}
+</style>
